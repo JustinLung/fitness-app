d7b5af36ae9ef7673fa5044babff57af882eef48
JustinLung <justinlung77@gmail.com>
2023-01-11 18:04:20 +0100

    removed login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,38 +12,39 @@ import { theme } from "../styles/theme";
 
 export default function Login() {
     return (
-        <Flex flexDir="column" maxW="90vw" mx="auto">
-            <Image src="/assets/images/hero-image.svg" w="15rem" mx="auto" />
-            <Heading textAlign="center">Welcome to Fitcheck!</Heading>
-            <Text textAlign="center">To improve yourself by sharing!</Text>
-            <Stack as="form" spacing={3} mt={6}>
-                <InputGroup>
-                    <Input
-                        type="email"
-                        placeholder="Email"
-                        _placeholder={{ color: theme.colors.black }}
-                        backgroundColor={theme.colors.grey}
-                    />
-                </InputGroup>
-                <InputGroup>
-                    <Input
-                        type="password"
-                        placeholder="Password"
-                        _placeholder={{ color: theme.colors.black }}
-                        backgroundColor={theme.colors.grey}
-                    />
-                </InputGroup>
-                <InputGroup>
-                    <Input
-                        type="submit"
-                        backgroundColor={theme.colors.black}
-                        color={theme.colors.white}
-                        value="Login"
-                    />
-                </InputGroup>
-                <Link textAlign="center">Forgot Password?</Link>
-                <Text textAlign="center">Don't have an account? <Link>Register</Link></Text>
-            </Stack>
-        </Flex >
+        <Text>Login Page</Text>
+        // <Flex flexDir="column" maxW="90vw" mx="auto">
+        //     <Image src="/assets/images/hero-image.svg" w="15rem" mx="auto" />
+        //     <Heading textAlign="center">Welcome to Fitcheck!</Heading>
+        //     <Text textAlign="center">To improve yourself by sharing!</Text>
+        //     <Stack as="form" spacing={3} mt={6}>
+        //         <InputGroup>
+        //             <Input
+        //                 type="email"
+        //                 placeholder="Email"
+        //                 _placeholder={{ color: theme.colors.black }}
+        //                 backgroundColor={theme.colors.grey}
+        //             />
+        //         </InputGroup>
+        //         <InputGroup>
+        //             <Input
+        //                 type="password"
+        //                 placeholder="Password"
+        //                 _placeholder={{ color: theme.colors.black }}
+        //                 backgroundColor={theme.colors.grey}
+        //             />
+        //         </InputGroup>
+        //         <InputGroup>
+        //             <Input
+        //                 type="submit"
+        //                 backgroundColor={theme.colors.black}
+        //                 color={theme.colors.white}
+        //                 value="Login"
+        //             />
+        //         </InputGroup>
+        //         <Link textAlign="center">Forgot Password?</Link>
+        //         <Text textAlign="center">Don't have an account? <Link>Register</Link></Text>
+        //     </Stack>
+        // </Flex >
     );
 }
