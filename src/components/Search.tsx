532e8f7f4b0a951819ed2e5be38f70f7115e1ab0
Justin Lung <justin.lung@hva.nl>
2023-01-12 17:27:27 +0100

    Image component removed from children prop

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -9,7 +9,7 @@ export default function Search({ searchPlaceholder }: SearchInterface) {
     return (
         <>
             <InputGroup maxW="90vw" mx="auto" my={5}>
-                <InputLeftElement children={<Image src="/assets/icons/search-icon.svg" />} h="full" ></InputLeftElement>
+                <InputLeftElement h="full" ><Image src="/assets/icons/search-icon.svg" /></InputLeftElement>
                 <Input type="text" placeholder={searchPlaceholder} _placeholder={{ color: theme.colors.black }} bg={theme.colors.lightgrey} />
             </InputGroup>
         </>
