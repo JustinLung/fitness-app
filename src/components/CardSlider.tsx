657dd980eb775028fd879513fd0cc98662b37996
Justin Lung <justin.lung@hva.nl>
2023-01-14 17:15:37 +0100

    added scroll indicators

--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -15,47 +15,57 @@ export default function CardSlider({
   cardSectionLink
 }: CardSectionProps) {
   return (
-    <>
-      <Box as="section" maxW="90vw" mx="auto">
-        <Flex
-          as="header"
-          justifyContent="space-between"
-          alignItems="center"
-          py={3}
-        >
-          <Heading as="h2" size="lg">
-            {cardSectionHeading}
-          </Heading>
-          <NextLink href={cardSectionLink}>
-            <Link
-              backgroundColor={theme.colors.orange}
-              color={theme.colors.white}
-              border={`1px solid ${theme.colors.orange}`}
-              borderRadius="2rem"
-              px="1.5rem"
-              py=".2rem"
-              _hover={{
-                textDecor: "none",
-                color: theme.colors.orange,
-                backgroundColor: "transparent",
-                border: "1px solid",
-                borderColor: theme.colors.orange
-              }}
-            >
-              View All
-            </Link>
-          </NextLink>
-        </Flex>
-        <Flex
-          gap="1rem"
-          overflowY="auto"
-          overscrollBehaviorX="contain"
-          scrollSnapType="x mandatory"
-          py={2}
-        >
-          {children}
-        </Flex>
-      </Box>
-    </>
+    <Box as="section" maxW="90vw" mx="auto">
+      <Flex
+        as="header"
+        justifyContent="space-between"
+        alignItems="center"
+        py={3}
+      >
+        <Heading as="h2" size="lg">
+          {cardSectionHeading}
+        </Heading>
+        <NextLink href={cardSectionLink}>
+          <Link
+            backgroundColor={theme.colors.orange}
+            color={theme.colors.white}
+            border={`1px solid ${theme.colors.orange}`}
+            borderRadius="2rem"
+            px="1.5rem"
+            py=".2rem"
+            _hover={{
+              textDecor: "none",
+              color: theme.colors.orange,
+              backgroundColor: "transparent",
+              border: "1px solid",
+              borderColor: theme.colors.orange
+            }}
+          >
+            View All
+          </Link>
+        </NextLink>
+      </Flex>
+      <Flex
+        gap="1rem"
+        overflowY="auto"
+        overscrollBehaviorX="contain"
+        scrollSnapType="x mandatory"
+        css={{
+          '&::-webkit-scrollbar': {
+            height: '.3rem',
+          },
+          '&::-webkit-scrollbar-track': {
+            height: '.3rem',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: theme.colors.black,
+            borderRadius: '2rem',
+          },
+        }}
+        py={2}
+      >
+        {children}
+      </Flex>
+    </Box>
   );
 }
