cb8f20b0942f527fed73808d7ca73c43831b85a1
Justin Lung <justin.lung@hva.nl>
2023-01-14 17:11:16 +0100

    added border to navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Container, List, ListItem, Image, Link } from "@chakra-ui/react";
+import { Container, List, ListItem, Image, Link, color } from "@chakra-ui/react";
 import NextLink from 'next/link'
 import { useRouter } from "next/router";
 import { theme } from "../styles/theme";
@@ -6,135 +6,139 @@ import { theme } from "../styles/theme";
 export default function Navigation() {
   const router = useRouter();
   return (
-    <Container
-      as="nav"
-      pos="fixed"
-      bottom={0}
-      left={0}
-      right={0}
-      py={3}
-      pb="calc(1em + env(safe-area-inset-bottom))"
-      bg={theme.colors.white}
-      w="100%"
-    >
-      <List display="flex" justifyContent="space-around" mx="auto">
-        <ListItem>
-          <NextLink href="/">
-            <Link
-              _hover={{ textDecor: "none" }}
-              _after={{
-                content: '"Home"',
-                fontSize: ".8rem",
-                opacity:
-                  router.pathname == "/"
-                    ? "1"
-                    : ".5",
-              }}
-              display="flex"
-              flexDir="column"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Image
-                src="/assets/icons/home-icon.svg"
-                alt="Home"
-                opacity={
-                  router.pathname == "/"
-                    ? "1"
-                    : ".5"
-                }
-              />
-            </Link>
-          </NextLink>
-        </ListItem>
-        <ListItem>
-          <NextLink href="/workouts">
-            <Link
-              _hover={{ textDecor: "none" }}
-              _after={{
-                content: '"Workouts"',
-                fontSize: ".8rem",
-                opacity:
-                  router.pathname == "/workouts"
-                    ? "1"
-                    : ".5",
-              }}
-              display="flex"
-              flexDir="column"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Image
-                alt="Workouts"
-                src="/assets/icons/barbell-icon.svg"
-                opacity={
-                  router.pathname == "/workouts"
-                    ? "1"
-                    : ".5"
-                }
-              />
-            </Link>
-          </NextLink>
-        </ListItem>
-        <ListItem>
-          <NextLink href="/meals">
-            <Link
-              _hover={{ textDecor: "none" }}
-              _after={{
-                content: '"Recipes"',
-                fontSize: ".8rem",
-                opacity:
-                  router.pathname == "/meals"
-                    ? "1"
-                    : ".5",
-              }}
-              display="flex"
-              flexDir="column"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Image
-                alt="Meals"
-                src="/assets/icons/apple-icon.svg"
-                opacity={
-                  router.pathname == "/meals"
-                    ? "1"
-                    : ".5"
-                }
-              />
-            </Link>
-          </NextLink>
-        </ListItem>
-        <ListItem>
-          <NextLink href="/profile">
-            <Link
-              _after={{
-                content: '"Profile"',
-                fontSize: ".8rem",
-                opacity:
-                  router.pathname == "/profile"
-                    ? "1"
-                    : ".5",
-              }}
-              _hover={{ textDecor: "none" }}
-              display="flex"
-              flexDir="column"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Image
-                alt="Profile"
-                src="/assets/icons/user-icon.svg"
-                opacity={
-                  router.pathname == "/profile"
-                    ? "1"
-                    : ".5"
-                }
-              />
-            </Link>
-          </NextLink>
-        </ListItem>
-      </List>
-    </Container>
+    <>
+      <Container
+        as="nav"
+        pos="fixed"
+        bottom={0}
+        left={0}
+        right={0}
+        py={3}
+        pb="calc(1em + env(safe-area-inset-bottom))"
+        bg={theme.colors.white}
+        w="100%"
+        borderTop="2px solid"
+        borderColor={theme.colors.lightgrey}
+      >
+        <List display="flex" justifyContent="space-around" mx="auto">
+          <ListItem>
+            <NextLink href="/">
+              <Link
+                _hover={{ textDecor: "none" }}
+                _after={{
+                  content: '"Home"',
+                  fontSize: ".8rem",
+                  opacity:
+                    router.pathname == "/"
+                      ? "1"
+                      : ".5",
+                }}
+                display="flex"
+                flexDir="column"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Image
+                  src="/assets/icons/home-icon.svg"
+                  alt="Home"
+                  opacity={
+                    router.pathname == "/"
+                      ? "1"
+                      : ".5"
+                  }
+                />
+              </Link>
+            </NextLink>
+          </ListItem>
+          <ListItem>
+            <NextLink href="/workouts">
+              <Link
+                _hover={{ textDecor: "none" }}
+                _after={{
+                  content: '"Workouts"',
+                  fontSize: ".8rem",
+                  opacity:
+                    router.pathname == "/workouts"
+                      ? "1"
+                      : ".5",
+                }}
+                display="flex"
+                flexDir="column"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Image
+                  alt="Workouts"
+                  src="/assets/icons/barbell-icon.svg"
+                  opacity={
+                    router.pathname == "/workouts"
+                      ? "1"
+                      : ".5"
+                  }
+                />
+              </Link>
+            </NextLink>
+          </ListItem>
+          <ListItem>
+            <NextLink href="/meals">
+              <Link
+                _hover={{ textDecor: "none" }}
+                _after={{
+                  content: '"Recipes"',
+                  fontSize: ".8rem",
+                  opacity:
+                    router.pathname == "/meals"
+                      ? "1"
+                      : ".5",
+                }}
+                display="flex"
+                flexDir="column"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Image
+                  alt="Meals"
+                  src="/assets/icons/apple-icon.svg"
+                  opacity={
+                    router.pathname == "/meals"
+                      ? "1"
+                      : ".5"
+                  }
+                />
+              </Link>
+            </NextLink>
+          </ListItem>
+          <ListItem>
+            <NextLink href="/profile">
+              <Link
+                _after={{
+                  content: '"Profile"',
+                  fontSize: ".8rem",
+                  opacity:
+                    router.pathname == "/profile"
+                      ? "1"
+                      : ".5",
+                }}
+                _hover={{ textDecor: "none" }}
+                display="flex"
+                flexDir="column"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Image
+                  alt="Profile"
+                  src="/assets/icons/user-icon.svg"
+                  opacity={
+                    router.pathname == "/profile"
+                      ? "1"
+                      : ".5"
+                  }
+                />
+              </Link>
+            </NextLink>
+          </ListItem>
+        </List>
+      </Container>
+    </>
   );
 }
